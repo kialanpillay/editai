fe784f7c5817a3043ebd8bc91c7f98a51b875842
Kialan Pillay <kialanpillay@gmail.com>
2023-02-04 15:15:47 +0000

    Add project structure

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router history={history}>
+        <div className={"App"}>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ web/src/pages/Landing.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
+import { initConnection, initTerra } from "terra-react";
+
+const Landing = () => {
+  useEffect(() => {
+    const init = async () => {
+      await initTerra("ichackflatline-dev-lB5aohCxQY");
+      await initConnection(
+        [Connections.APPLE_HEALTH],
+        "0c9e099cf990db13decf253d451729010a8ce521082421b660fe194d7d96e0d7",
+        true
+      );
+    };
+
+    void init();
+  });
+
+  return (
+    <Container className={"page"}>
+      <div>
+        <h1>Connect Device</h1>
+        <Button>Connect</Button>
+      </div>
+    </Container>
+  );
+};
+
+export default Landing;
